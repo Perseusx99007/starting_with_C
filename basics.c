16b73aa4284b5e819a7addd8a248d5cf8be69c15
Perseus <ratulsaha99@gmail.com>
2024-02-09 01:23:11 +0530

    Updated program showing the basics of C

---
+++ basics.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    //to print...
+
+    /*printf("Hello, world!\n");*/
+
+
+    //to take an input from the user and print it...
+
+    /*string answer = get_string("What's your name? ");
+    printf("Hello!!!, %s\n", answer);*/
+
+
+    //to take 2 inputs from the user and check whether one is greater than the other or the same...
+
+    /*int x = get_int("What is the value of x? ");
+    int y = get_int("What is the value of y? ");
+
+    if (x < y)
+    {
+        printf("The value of y is greater than x. \n");
+    }
+    else
+    if (x > y)
+    {
+        printf("The value of x is greater than y. \n");
+    }
+    else
+    {
+        printf("The value of both x and y are the same. \n");
+    }*/
+
+
+    //using "char" and "||(or)"
+
+    /*char c = get_char("Do you agree? ");
+
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed \n");
+    }
+    else
+    if (c == 'n' || c == 'N')
+    {
+        printf("Not agreed. \n");
+    }*/
+
+    //using while loop
+
+    /*int i = 3;
+
+    while (i > 0)
+    {
+        printf("Hello!!! \n");
+        i--;
+    }*/
+
+
+    //using for loop
+
+    /*for(int i = 0; i < 3; i++)
+    {
+        printf("Hello!!! \n");
+    }*/
+
+    //getting started with functions
+
+
+    /*void hello(int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+        printf("Hello!!! \n");
+        }
+    }
+
+    hello(40);*/
+
+
+    // using functions to calculate
+
+    /*int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    int x = get_int("Enter first number : ");
+    int y = get_int("Enter second number : ");
+
+    printf("%i/n", add(x,y));*/
+}
